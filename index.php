082f284a9aec33a2aa15c67823fb9a569f5fd56f
Lawtrel <kazemarus@outlook.com>
2024-07-29 12:33:50 -0300

    update

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ DATA:20/07/2024
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="Userpagestyle.css">
+    <link rel="stylesheet" href="index.css">
     <!--Link dos icones-->
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="icon" href="favicon.png" type="image/x-icon"/>
